18e07ef4b4f3626ad66e310d073b338aa973a0a4
sjf1616 <sjf16s20@gmail.com>
2025-08-15 21:23:20 -0300

    feat: update docstring and reformulate paths

--- src/simulador.py
+++ src/simulador.py
@@ -1,12 +1,12 @@
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from models.model import AgenteSimulador
+from data.carrega_data import CarregaData
 import random
 import pandas as pd
 
 def choice_action():
-    agente = AgenteSimulador()
+    agente = CarregaData()
 
     for _ in range(0, 1000):
         acao = agente.escolher_acao()
@@ -14,9 +14,20 @@ def choice_action():
         agente.registrar_recompensa(acao, recompensa)
 
 def kill_agent():
-    agente = AgenteSimulador()
-    agente.kill_agent()
-    print(f'Agente resetado com sucesso!')
+    temp_pass = 0000
+    o = str(input('Deseja realmente apagar todos os dados do dataset? [S/N]: ')).lower()
+    pass_w = int(input('Password: '))
+    if pass_w == temp_pass:
+        match o[0]:
+            case "s":
+                agente = CarregaData()
+                agente.kill_agent()
+                print(f'Agente resetado com sucesso!')
+            case "n":
+                main()
+    else:
+        print('\033[31m(Incorret Password, try again)\033[m')
+        main()
 
 def tittle(text):
     print('-'*30)
@@ -25,6 +36,10 @@ def tittle(text):
 
 def public():
     tittle('Reward Simulator')
+    list_option = ['training basic model', 'reset agente', 'show model', 'exit']
+    for e, _ in enumerate(list_option):
+        print(f'{e+1} - {_.title()}')
+    return list_option
 
 def show_model():
     df_data = pd.DataFrame(data = pd.read_json('data/data.json'))
@@ -33,12 +48,7 @@ def show_model():
     df_data_invertido.columns = ["Vezes", "Soma", "Media", "Esforço"]
     print(df_data_invertido)
 
-def option():
-    list_option = ['training basic model', 'reset agente', 'show model']
-    for e, _ in enumerate(list_option):
-        print(f'{e+1} - {_.title()}')
-    option_choice = input('Choice your option: ').lower()
-
+def option(list_option, option_choice):
     while option_choice not in list_option:
         option_choice = input('\033[31m(OPTION NOT FOUND, TRY AGAIN)\033[m Choice your option: ').strip().lower()
 
@@ -54,9 +64,13 @@ def option():
         case "show model":
             show_model()
 
+        case "exit":
+            exit
+
 def main():
-    public()
-    option()
+    lista = public()
+    option_choice = input('Choice your option: ').lower()
+    option(lista, option_choice)
 
 if __name__ == "__main__":
     main()
